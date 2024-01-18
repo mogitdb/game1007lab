87f62711dc5ada29194a520d4250173d17973fbb
MechaOphidian <126343804+MechaOphidian@users.noreply.github.com>
2024-01-18 13:04:21 -0500

    Added sup gang

--- Assets/Lab.cs
+++ Assets/Lab.cs
@@ -7,7 +7,7 @@ public class Lab : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Sup Gang");
     }
 
     // Update is called once per frame
